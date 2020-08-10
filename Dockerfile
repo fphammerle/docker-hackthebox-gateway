d6541b1dd6f6a17d23380587e3d86f996aae0956
Fabian Peter Hammerle <fabian@hammerle.me>
2020-08-10 19:44:24 +0200

    make openvpn config path configurable

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ ARG OPENVPN_PACKAGE_VERSION=2.4.9-r0
 RUN apk add --no-cache openvpn=$OPENVPN_PACKAGE_VERSION
 
 VOLUME /vpn-config
+ENV OPENVPN_CONFIG_PATH=/vpn-config/hackthebox.ovpn
 COPY ./vpn.sh /vpn.sh
 CMD ["/vpn.sh"]
--- vpn.sh
+++ vpn.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-set -ex
+set -eux
 
 iptables -P FORWARD DROP
 iptables -A FORWARD -i eth0 -o tun0 -d 10.10.10.0/24 -j ACCEPT
 iptables -A FORWARD -i tun0 -o eth0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
 
-openvpn --config /vpn-config/hackthebox.ovpn \
+openvpn --config "$OPENVPN_CONFIG_PATH" \
     --user openvpn --group openvpn
