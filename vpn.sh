a0876e946f59eb114f4e15f777380bb4578eb21b
Fabian Peter Hammerle <fabian@hammerle.me>
2021-09-01 07:33:11 +0200

    replace shell with openvpn process (instead of creating subprocess)

--- vpn.sh
+++ vpn.sh
@@ -7,5 +7,5 @@ iptables -A FORWARD -i eth0 -o tun0 -d 10.10.10.0/24 -j ACCEPT
 iptables -A FORWARD -i tun0 -o eth0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
 
-openvpn --config "$OPENVPN_CONFIG_PATH" \
+exec openvpn --config "$OPENVPN_CONFIG_PATH" \
     --user openvpn --group openvpn
